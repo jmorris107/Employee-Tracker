a29bd6f2802553a8d5e834d2b50b9a2c8c6b1a8b
Jennifer Morris <jenniferinbaranoseidou@gmail.com>
2020-10-28 14:40:11 -0400

    prepared base

---
+++ index.js
@@ -0,0 +1,19 @@
+//require inquirer, mysql
+
+// make a connection
+
+//main menue
+
+//ask
+
+
+// Add departments, roles, employees
+// view departments, roles, employees
+//Update employee roles
+
+// function Add
+
+// function view
+
+// function update
+//
